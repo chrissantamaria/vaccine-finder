a1191463e04f2684792371cc2b9083f54a680d56
Chris Santamaria <chris@santamaria.me>
2021-02-14 13:56:51 -0500

    Add cron task to check every minute

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
+require('console-stamp')(console, {
+  label: false,
+});
+const cron = require('node-cron');
 
 const checkLocations = require('./checkLocations');
 
-checkLocations().catch(console.error);
+console.log('Server started, will check every minute');
+
+// every minute
+cron.schedule('*/1 * * * *', () => {
+  console.log('Checking...');
+  checkLocations().catch(console.error);
+});
